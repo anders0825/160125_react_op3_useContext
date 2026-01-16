a8f950490e1476925e36596c6417ed29f6fd0f79
anders0825 <gah082025@gmail.com>
2026-01-16 14:49:48 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/book-landing-page/",
+  base: "/160125_react_op3_useContext/",
 });
 
 /*
